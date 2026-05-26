1d9da335039789cb30c444fbf39ee304fb2ef18a
glriadomenica-debug <glriadomenica@gmail.com>
2026-05-26 20:33:26 +0700

    first commit

---
+++ app/Models/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'type',
+        'amount',
+        'description',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
