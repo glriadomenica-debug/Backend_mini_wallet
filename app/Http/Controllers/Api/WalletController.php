5be1b1f124a400edac4fcf1c47349be26973313a
glriadomenica-debug <glriadomenica@gmail.com>
2026-05-26 21:23:03 +0700

    wallet controller

--- app/Http/Controllers/Api/WalletController.php
+++ app/Http/Controllers/Api/WalletController.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WalletController extends Controller
 {
-    //
+    public function balance()
+    {
+        $wallet = Wallet::where('user_id', Auth::id())->first();
+        return response()->json([
+            'balance' => $wallet->balance
+        ]);
+    }
+
+    public function topup(Request $request)
+    {
+        $request->validate([
+            'amount' => 'required|numeric|min:1'
+        ]);
+
+        $wallet = Wallet::where('user_id', Auth::id())->first();
+        $wallet->balance += $request->amount;
+        $wallet->save();
+
+        Transaction::create([
+            'sender_id' => null,
+            'receiver_id' => Auth::id(),
+            'type' => 'topup',
+            'amount' => $request->amount,
+            'description' => 'Topup balance'
+        ]);
+        return response()->json([
+            'message' => 'Topup successful',
+            'balance' => $wallet->balance
+        ]);
+    }
 }
